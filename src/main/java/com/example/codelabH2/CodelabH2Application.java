fa4fe978c6310bc22416c51ac9c96779c4de858f
ykvelandia <velandia416@gmail.com>
2024-02-13 21:59:38 -0500

    first commit

---
+++ src/main/java/com/example/codelabH2/CodelabH2Application.java
@@ -0,0 +1,13 @@
+package com.example.codelabH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelabH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelabH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codelabH2/CodelabH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codelabH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
